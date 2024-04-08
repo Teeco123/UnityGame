b1f999e6fdf7fcc5ff82ff3d3759a456c2d99802
Kacper <kacper.gajko1@gmail.com>
2024-04-08 19:57:10 +0200

    Naprawiony blad z kompilacja buildu

--- Assets/Scripts/Dialogues/DialogueVariables.cs
+++ Assets/Scripts/Dialogues/DialogueVariables.cs
@@ -9,11 +9,9 @@ public class DialogueVariables : MonoBehaviour
 {
     private Dictionary<string, Ink.Runtime.Object> variables;
 
-    public DialogueVariables(string globalsFilePath)
+    public DialogueVariables(TextAsset loadGlobalsJSON)
     {
-        string inkFileContents = File.ReadAllText(globalsFilePath);
-        Ink.Compiler compiler = new Ink.Compiler(inkFileContents);
-        Story globalVariablesStory = compiler.Compile();
+        Story globalVariablesStory = new Story(loadGlobalsJSON.text);
 
         variables = new Dictionary<string, Ink.Runtime.Object>();
         foreach (string name in globalVariablesStory.variablesState)
