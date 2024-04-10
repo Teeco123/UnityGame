96d8b580654394799660738306f649dd8b0caaa2
Kacper <kacper.gajko1@gmail.com>
2024-04-10 19:53:56 +0200

    Zmiana sceny tylko przy nacisnieciu przycisku

--- Assets/Scripts/SceneChanging/SceneTransition.cs
+++ Assets/Scripts/SceneChanging/SceneTransition.cs
@@ -6,12 +6,25 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     public string sceneToLoad;
+    public bool canEnter;
     public Vector3 playerPosition;
     public VectorValue playerStorage;
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.CompareTag("Player") && !collider.isTrigger)
+        if (collider.CompareTag("Player"))
+            canEnter = true;
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+            canEnter = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canEnter)
         {
             playerStorage.initialValue = playerPosition;
             SceneManager.LoadSceneAsync(sceneToLoad);
