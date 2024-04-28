54fa09e00dea7cdedb4175e69ace544a41b87bea
Kacper <kacper.gajko1@gmail.com>
2024-04-28 17:16:16 +0200

    dodano automatyczne strzelanie

--- Assets/Scripts/Player/Shooting.cs
+++ Assets/Scripts/Player/Shooting.cs
@@ -6,18 +6,41 @@ public class Shooting : MonoBehaviour
 {
     public int damage = 1;
     public float range = 50;
+    public float fireRate = 15;
+
+    public enum WeaponType
+    {
+        singleFire,
+        Automatic
+    }
+
+    [SerializeField]
+    private WeaponType weaponType;
 
     public Camera playerCamera;
-
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (weaponType == WeaponType.Automatic)
         {
-            Shoot();
+            if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+                Shoot();
+            }
+        }
+        else if (weaponType == WeaponType.singleFire)
+        {
+            if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+                Shoot();
+            }
         }
     }
 
