44e2e25310008c1d3bbab456260aa56e6398835b
Kacper <kacper.gajko1@gmail.com>
2024-04-30 19:56:58 +0200

    comments for GameData

--- Assets/Scripts/DataSaving/GameData.cs
+++ Assets/Scripts/DataSaving/GameData.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 [System.Serializable]
 public class GameData
 {
+    //All variables for saving
     public Vector3 playerPosition;
     public string currentVariables;
     public List<InventoryModel> inventory;
 
+    //Default data for variables
     public GameData()
     {
         playerPosition = new Vector3(0, 1, 0);
