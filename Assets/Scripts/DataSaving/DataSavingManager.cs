d334600e53ee01b17b17d597b070f45d4892c690
Kacper <kacper.gajko1@gmail.com>
2024-04-30 19:45:26 +0200

    Comments for Data Saving Manager

--- Assets/Scripts/DataSaving/DataSavingManager.cs
+++ Assets/Scripts/DataSaving/DataSavingManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -17,6 +16,7 @@ public class DataSavingManager : MonoBehaviour
 
     private void Awake()
     {
+        //Checking if Data Saving Manager is on the scene
         if (instance != null)
         {
             Debug.LogError("There is Data Saving Manager already!");
@@ -27,26 +27,31 @@ public class DataSavingManager : MonoBehaviour
 
     private void OnEnable()
     {
+        //Handling Loading when scene in unloaded
         SceneManager.sceneLoaded += OnSceneLoaded;
         SceneManager.sceneUnloaded += OnSceneUnloaded;
     }
 
     private void OnDisable()
     {
+        //Handling saving when scene in unloaded
         SceneManager.sceneLoaded -= OnSceneLoaded;
         SceneManager.sceneUnloaded -= OnSceneUnloaded;
     }
 
+    //Saving game when loading scene
     public void OnSceneUnloaded(Scene scene)
     {
         SaveGame();
     }
 
+    //Clearing save file (Not Working 💀💀)
     public void NewGame()
     {
         this.gameData = new GameData();
     }
 
+    //Loading game when loading scene
     public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         this.savingInterfaceObjects = FindAllSavingInterfaceObjects();
@@ -57,13 +62,16 @@ public class DataSavingManager : MonoBehaviour
 
     public void LoadGame()
     {
-        this.gameData = dataHandler.Load();
+        this.gameData = dataHandler.Load(); //Passing gamedata from the file
+
         if (this.gameData == null)
         {
+            //Making new game in case no save file is found
             Debug.Log("No data Found");
             NewGame();
         }
 
+        //Loading variables from save file for every script with SavingInterface in it
         foreach (SavingInterface savingInterfaceObj in savingInterfaceObjects)
         {
             savingInterfaceObj.LoadData(gameData);
@@ -72,19 +80,23 @@ public class DataSavingManager : MonoBehaviour
 
     public void SaveGame()
     {
+        //Saving variables to save file for every script with SavingInterface in it
         foreach (SavingInterface savingInterfaceObj in savingInterfaceObjects)
         {
             savingInterfaceObj.SaveData(gameData);
         }
 
+        //Passing data to file
         dataHandler.Save(gameData);
     }
 
+    //Saving when leaving game
     private void OnApplicationQuit()
     {
         SaveGame();
     }
 
+    //Finding all Saving interface on the scene
     private List<SavingInterface> FindAllSavingInterfaceObjects()
     {
         IEnumerable<SavingInterface> savingInterfaceObjects = FindObjectsOfType<MonoBehaviour>()
