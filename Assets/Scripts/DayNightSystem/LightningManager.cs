a9bbe5e79022e7880d2511f4cd148298a843cb34
Kacper <kacper.gajko1@gmail.com>
2024-06-23 14:00:07 +0200

    Skybox using procedular skybox

--- Assets/Scripts/DayNightSystem/LightningManager.cs
+++ Assets/Scripts/DayNightSystem/LightningManager.cs
@@ -51,6 +51,7 @@ public class LightningManager : MonoBehaviour
         //Setting color based on gradients from our preset
         RenderSettings.ambientLight = preset.ambientColor.Evaluate(timePercent);
         RenderSettings.fogColor = preset.fogColor.Evaluate(timePercent);
+        skyboxMaterial.SetColor("_SkyTint", preset.skyboxColor.Evaluate(timePercent));
 
         if (directionalLight != null)
         {
