7eb595a91c6581e7fa68223bf2fe4580a22771d0
Kacper <kacper.gajko1@gmail.com>
2024-06-25 01:31:27 +0200

    moving upgraded character

--- Assets/Scripts/Player/PlayerControls/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerControls/PlayerCamera.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     float xRotation = 0f;
+    float yRotation = 0f;
 
+    public Transform head;
     public Transform body;
     public float mouseSensitivity = 100f;
 
@@ -13,7 +15,7 @@ public class PlayerCamera : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         //Stops moving camera when dialogue is playing
         if (DialogueManager.Getinstance().dialogueIsPlaying || SceneTransition.triggeredEnter)
@@ -27,10 +29,12 @@ public class PlayerCamera : MonoBehaviour
 
         //Locks player from moving camera more than 90 degrees up and down
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -80f, 75f);
+
+        yRotation += mouseX;
+        yRotation = Mathf.Clamp(yRotation, -80f, 80f);
 
         //Rotates camera
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        body.Rotate(Vector3.up * mouseX);
+        head.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
     }
 }
