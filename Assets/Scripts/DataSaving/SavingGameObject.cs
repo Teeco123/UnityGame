f4e6b041218a25a36b06df9a7469295e72acc660
Kacper <kacper.gajko1@gmail.com>
2024-06-27 16:51:15 +0200

    Update SavingGameObject.cs

--- Assets/Scripts/DataSaving/SavingGameObject.cs
+++ Assets/Scripts/DataSaving/SavingGameObject.cs
@@ -5,22 +5,15 @@ using UnityEngine;
 public class SavingGameObject : MonoBehaviour
 {
     public string guid = System.Guid.NewGuid().ToString();
-    static bool isApplicationQuitting = false;
 
-    void Start()
+    void Awake()
     {
         if (ES3.KeyExists("GameObject: " + guid))
             Destroy(this.gameObject);
     }
 
-    void OnDestroy()
-    {
-        if (gameObject.scene.isLoaded && !isApplicationQuitting)
-            ES3.Save<bool>("GameObject: " + guid, true);
-    }
-
     void OnApplicationQuit()
     {
-        isApplicationQuitting = true;
+        ES3.Save<bool>("GameObject: " + guid, true);
     }
 }
