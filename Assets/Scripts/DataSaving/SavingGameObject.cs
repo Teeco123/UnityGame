0f9dcde9dc781f8a8cd07c82816db26e7b00f11b
Kacper <kacper.gajko1@gmail.com>
2024-06-27 17:02:55 +0200

    Revert "Update SavingGameObject.cs"
    This reverts commit f4e6b041218a25a36b06df9a7469295e72acc660.

--- Assets/Scripts/DataSaving/SavingGameObject.cs
+++ Assets/Scripts/DataSaving/SavingGameObject.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class SavingGameObject : MonoBehaviour
 {
     public string guid = System.Guid.NewGuid().ToString();
+    static bool isApplicationQuitting = false;
 
-    void Awake()
+    void Start()
     {
         if (ES3.KeyExists("GameObject: " + guid))
             Destroy(this.gameObject);
     }
 
+    void OnDestroy()
+    {
+        if (gameObject.scene.isLoaded && !isApplicationQuitting)
+            ES3.Save<bool>("GameObject: " + guid, true);
+    }
+
     void OnApplicationQuit()
     {
-        ES3.Save<bool>("GameObject: " + guid, true);
+        isApplicationQuitting = true;
     }
 }
